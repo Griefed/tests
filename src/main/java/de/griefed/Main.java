47bce97dba049014d809b28868ad09c44ff23e1a
Griefed <griefed@griefed.de>
2022-07-16 10:58:23 +0200

    no message

--- src/main/java/de/griefed/Main.java
+++ src/main/java/de/griefed/Main.java
@@ -9,15 +9,27 @@ public class Main {
 
   public static void main(String[] args) throws ZipException {
 
-    List<FileHeader> headersValid = new ZipFile("Survive_Create_Prosper_4_valid.zip").getFileHeaders();
-    List<FileHeader> headersInvalid = new ZipFile("Survive_Create_Prosper_4_invalid.zip").getFileHeaders();
+    List<FileHeader> headersValid =
+        new ZipFile("Survive_Create_Prosper_4_valid.zip").getFileHeaders();
+    List<FileHeader> headersInvalid =
+        new ZipFile("Survive_Create_Prosper_4_invalid.zip").getFileHeaders();
 
     System.out.println("Valid");
-    headersValid.forEach(fileHeader -> System.out.println(fileHeader.getFileName()));
+    headersValid.forEach(
+        fileHeader -> {
+          if (!fileHeader.isDirectory()) {
+            System.out.println("  " + fileHeader.getFileName());
+          }
+        });
 
     System.out.println();
 
     System.out.println("Invalid");
-    headersInvalid.forEach(fileHeader -> System.out.println(fileHeader.getFileName()));
+    headersInvalid.forEach(
+        fileHeader -> {
+          if (!fileHeader.isDirectory()) {
+            System.out.println("  " + fileHeader.getFileName());
+          }
+        });
   }
 }
