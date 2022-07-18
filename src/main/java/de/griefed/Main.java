4236bc516c7bc7312f680b83ee49f54be99dd6da
Griefed <griefed@griefed.de>
2022-07-18 20:47:32 +0200

    no message

--- src/main/java/de/griefed/Main.java
+++ src/main/java/de/griefed/Main.java
@@ -1,18 +1,26 @@
 package de.griefed;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.FileHeader;
+import org.apache.commons.io.FileUtils;
 
 public class Main {
 
   public static void main(String[] args) throws ZipException {
 
-    List<FileHeader> headersValid =
-        new ZipFile("Survive_Create_Prosper_4_valid.zip").getFileHeaders();
-    List<FileHeader> headersInvalid =
-        new ZipFile("Survive_Create_Prosper_4_invalid.zip").getFileHeaders();
+    ZipFile valid = new ZipFile("Survive_Create_Prosper_4_valid.zip");
+    ZipFile invalid = new ZipFile("Survive_Create_Prosper_4_invalid.zip");
+    ZipFile jar = new ZipFile("ServerPackCreator-dev.jar");
+
+    List<FileHeader> headersValid = valid.getFileHeaders();
+    List<FileHeader> headersInvalid = invalid.getFileHeaders();
+    List<FileHeader> headersJar = jar.getFileHeaders();
+
+    String extractPreFix = "temp/";
 
     System.out.println("Valid");
     headersValid.forEach(
@@ -31,5 +39,40 @@ public class Main {
             System.out.println("  " + fileHeader.getFileName());
           }
         });
+
+    System.out.println();
+
+    System.out.println("ServerPackCreator");
+    headersJar.forEach(
+        fileHeader -> {
+          System.out.println("  " + fileHeader.getFileName());
+          if (fileHeader.getFileName().equals("BOOT-INF/lib/artemis-journal-2.19.1.jar")) {
+
+            File file = new File(extractPreFix + fileHeader.getFileName());
+
+            if (!file.exists()) {
+              try {
+
+                FileUtils.copyInputStreamToFile(jar.getInputStream(fileHeader), file);
+
+              } catch (IOException e) {
+                throw new RuntimeException(e);
+              }
+            }
+
+            try (ZipFile zipFile = new ZipFile(file)) {
+
+              System.out.println("  " + file.getName());
+              zipFile.getFileHeaders().forEach(fileHeader1 -> System.out.println("      " + fileHeader1));
+
+            } catch (IOException e) {
+              throw new RuntimeException(e);
+            }
+          }
+        });
+  }
+
+  private static void getFileFromJarInJar(ZipFile initialJar, String fileOrDirToGet, String... embeddedJars) {
+
   }
 }
